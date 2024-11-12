a6be5b759d05efea2d32c80cf54895e2b1cdef28
Aviad Roichman <aviadroichman@gmail.com>
2024-11-12 08:34:56 +0200

    settings..

--- library_django_backend_project/settings.py
+++ library_django_backend_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&=^=g+cs!=rri0d6zu)fa)jhz^szsc!dr__kedu+g!l9^xf_-7'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -159,7 +160,6 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=100)
 }
 
-from decouple import config
 
 DATABASES = {
     'default': {
