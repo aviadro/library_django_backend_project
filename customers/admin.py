2349d7baf4602b5f9ff0ac1766b758b52fa1fcd9
Aviad Roichman <aviadroichman@gmail.com>
2025-01-03 12:29:19 +0200

    admin user v2

--- customers/admin.py
+++ customers/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from customers.models import Customer
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import Customer
 
-admin.site.register(User, UserAdmin)
+admin.site.register(Customer, UserAdmin)
 admin.site.register(Customer)
